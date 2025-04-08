1e2de32c9df1c60f1a1298fa2f6ee36d567769f9
Tito Julio González Padial <72378065+tgp034@users.noreply.github.com>
2025-04-08 08:53:22 +0000

    Excuses made

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let excuse = "";
+  excuse += who[Math.floor(Math.random()*who.length)] + " ";
+  excuse += action[Math.floor(Math.random()*action.length)] + " ";
+  excuse += what[Math.floor(Math.random()*what.length)] + " ";
+  excuse += when[Math.floor(Math.random()*when.length)];
+  document.querySelector("#excuse").innerHTML = excuse;
+  console.log(excuse);
 };
